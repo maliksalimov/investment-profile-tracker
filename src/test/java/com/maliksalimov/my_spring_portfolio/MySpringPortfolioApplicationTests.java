99293a286b03ac524c69133132045c67eb2ad774
maliksalimov <87946333+maliksalimov@users.noreply.github.com>
2026-03-06 17:53:21 +0400

    Initial commit and project is done

---
+++ src/test/java/com/maliksalimov/my_spring_portfolio/MySpringPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maliksalimov.my_spring_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
